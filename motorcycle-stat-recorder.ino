2bce7b036b8375b011e6d02ac8c7ccb4ef50d19b
travistomer <75449831+travistomer@users.noreply.github.com>
2020-12-03 14:14:25 -0600

    Add files via upload

---
+++ motorcycle-stat-recorder.ino
@@ -0,0 +1,102 @@
+
+// Basic demo for accelerometer readings from Adafruit LIS3DH
+
+#include <Wire.h>
+#include <SPI.h>
+#include <math.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+
+// Used for software SPI
+#define LIS3DH_CLK 13
+#define LIS3DH_MISO 12
+#define LIS3DH_MOSI 11
+// Used for hardware & software SPI
+#define LIS3DH_CS 10
+
+// Define vars
+float theta;
+
+// Create sensor object
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+// SETUP
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+  
+  Serial.println("LIS3DH test!");
+
+  // Check if accel is connected
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1) yield();
+  }
+  Serial.println("LIS3DH found!");
+
+  // Set range of measured G 
+  lis.setRange(LIS3DH_RANGE_8_G);   // 2, 4, 8 or 16 G!
+
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());
+  Serial.println("G");
+
+  // Set data rate
+  lis.setDataRate(LIS3DH_DATARATE_50_HZ);
+  Serial.print("Data rate set to: ");
+
+  // Gets set data rate
+  switch (lis.getDataRate()) {
+    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
+    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
+    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
+    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
+    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
+    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
+    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
+
+    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
+    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
+    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("16 Khz Low Power"); break;
+  }
+  
+}
+
+// LOOP
+void loop() {
+
+  // Get new sensor event
+  sensors_event_t event;
+  lis.getEvent(&event);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  Serial.print(" \tY: "); Serial.print(event.acceleration.y);
+  Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
+  Serial.print(" m/s^2 ");
+
+
+  // Calculate lean angle (x)
+  theta = atan(event.acceleration.x / (sqrt(event.acceleration.y * event.acceleration.x + event.acceleration.z * event.acceleration.z)));
+
+  // Convert to degrees
+  theta = theta * 180/3.14159;
+
+  // Display theta value
+  Serial.print("\tTheta: "); Serial.print(theta);
+
+
+  // Calculate angle using atan2
+  theta = atan2(event.acceleration.y, event.acceleration.z); //roll
+  // Convert to degrees
+  theta = theta * 180/3.14159;
+  
+  Serial.print("\tTheta (atan2): "); Serial.print(theta);
+  
+  Serial.println();
+
+}
+
+// Calculates lean angle from accelerometer readings 
+float calcAngle(){
+  
+}
